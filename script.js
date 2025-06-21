54cd98f4c1eda6b0a2f89aa0b2f94ac9a67e01d6
Ganesh Yadav <ganeshyadav1092002@gmail.com>
2025-06-21 14:39:00 +0530

    completed ToDo List project

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const todoList = document.querySelector(".todo-list");
 
 
 button.addEventListener("click", addTodo);
-
+todoList.addEventListener("click", deleteTodo);
 
 function addTodo(event){
    event.preventDefault();
@@ -13,11 +13,33 @@ function addTodo(event){
    todoDiv.classList.add("todo-container")
 
    const todoItem = document.createElement("li")
-   todoDiv.classList.add("todo-container")
+   todoItem.classList.add("todo-item")
+   todoItem.innerText = input.value;
+   todoDiv.appendChild(todoItem);
 
    const deletButton = document.createElement("button")
-   todoDiv.classList.add("todo-container")
+   deletButton.classList.add("delete-btn")
+   deletButton.innerHTML = `<i class="fa-solid fa-trash-can"></i>`
+   todoDiv.appendChild(deletButton);
 
-   const complete1button = document.createElement("button")
-   todoDiv.classList.add("todo-container")
+   const completeButton = document.createElement("button")
+   completeButton.classList.add("complete-btn")
+   completeButton.innerHTML= `<i class="fa-solid fa-circle-check"></i>`;
+   todoDiv.appendChild(completeButton);
+
+   todoList.appendChild(todoDiv);
+   input.value = "";
+}
+
+function deleteTodo(event){
+ 
+   const item = event.target;
+   if(item.classList[0] === "delete-btn"){
+     const delItem = item.parentElement;
+     delItem.remove();      
+   }
+   if(item.classList[0] === "complete-btn"){
+     const delItem = item.parentElement;
+     delItem.classList.toggle("completed"); 
+   }
 }
